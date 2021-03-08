549d836f3138f16875589b30d929b52e154e8f5c
Guigalaverna <guigalaverna@gmail.com>
2021-03-08 17:16:02 -0300

    New components

---
+++ src/components/HomeBackground.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/HomeBackground.module.css";
+
+export default function HomeBackground() {
+  return (
+    <div className={styles.homeBackgroundContainer}>
+      <img src="./Illus2.svg" />
+    </div>
+  );
+}
